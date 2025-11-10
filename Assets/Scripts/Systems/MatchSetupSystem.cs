8047fdb82f2267ab25e7e8b2ba41529cbb2415c6
cojjeeee <gi686273@gmail.com>
2025-11-10 14:14:03 +0800

    Mana System Added
    Mana System, Mana UI, Spending and Refilling Mana implemented.

--- Assets/Scripts/Systems/MatchSetupSystem.cs
+++ Assets/Scripts/Systems/MatchSetupSystem.cs
@@ -11,8 +11,13 @@ public class MatchSetupSystem : MonoBehaviour
     private void Start()
     {
         CardSystem.Instance.Setup(deckData);
-        DrawCardsGA drawCardsGA = new (5);
-        ActionSystem.Instance.Perform(drawCardsGA);
+        
+        RefillManaGA refillManaGA = new();
+        ActionSystem.Instance.Perform(refillManaGA, () =>
+        {
+            DrawCardsGA drawCardsGA = new(5);
+            ActionSystem.Instance.Perform(drawCardsGA);
+        });
     }
 
 }
